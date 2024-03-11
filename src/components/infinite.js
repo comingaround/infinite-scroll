543f642ce84bf0a6317ec89b514983250c9e86c2
comingaround <131857839+comingaround@users.noreply.github.com>
2024-03-11 16:46:02 +0200

    commit

--- src/components/infinite.js
+++ src/components/infinite.js
@@ -8,12 +8,13 @@ function Infinite() {
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
   const loader = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(entries => {
       const first = entries[0];
-      if (first.isIntersecting) {
+      if (first.isIntersecting && !isFetching) {
         fetchImages();
       }
     }, { threshold: 0.1 });
@@ -28,10 +29,12 @@ function Infinite() {
         observer.unobserve(currentLoader);
       }
     };
-  }, [page]);
+  }, [page, isFetching]);
 
   const fetchImages = () => {
-    console.log("------");
+    if (isFetching) return;
+
+    setIsFetching(true); 
     const tags = 'scifi';
     const perPage = 12;
     const URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${tags}&per_page=${perPage}&page=${page}&format=json&nojsoncallback=1&extras=url_l,url_m,url_s,owner_name&sort=interestingness-desc`;
@@ -53,7 +56,16 @@ function Infinite() {
           setHasMore(false);
         }
       })
-      .catch(error => console.log(error));
+      .catch(error => console.log(error))
+      .finally(() => setIsFetching(false));
+  };
+  const saveImageUrlToLocal = (url) => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteImages')) || [];
+    if (!favorites.includes(url)) {
+      favorites.push(url);
+      localStorage.setItem('favoriteImages', JSON.stringify(favorites));
+    }
+    console.log(url);
   };
 
   return (
@@ -62,16 +74,20 @@ function Infinite() {
       <div key={index} className="image-item">
         <img 
           src={img.src_l} 
-          srcSet={`${img.src_s} 1024w, ${img.src_m} 1440w, ${img.src_l} 2048w`} 
+          srcSet={`${img.src_s} 426w, ${img.src_m} 1024w, ${img.src_l} 1440w`} 
           sizes="(max-width: 426px) 100vw, (max-width: 1024px) 50vw, 33vw"
           alt={img.title}
           loading="lazy"
           data-testid="image-item"
         />
         <div className="credentials">
-            <p>Author: {img.author}</p>
-            <p>Title: {img.title}</p>
+            <span />
+            <section>
+              <h1>{img.title}</h1>
+              <h3>{img.author}</h3>
+            </section>
             <div className='favorite'>
+              <button onClick={() => saveImageUrlToLocal(img.src_l)}>Add to Favorite</button>
               <button>Favorite</button>
             </div>
         </div>
